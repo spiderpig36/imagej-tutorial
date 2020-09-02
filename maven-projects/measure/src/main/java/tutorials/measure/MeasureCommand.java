210861034cf94052a3b59435737688fe7719c2f1
Dorner Nic Alexander <nicalexander.dorner@students.bfh.ch>
2020-09-02 08:17:48 +0200

    measure

--- maven-projects/measure/src/main/java/tutorials/measure/MeasureCommand.java
+++ maven-projects/measure/src/main/java/tutorials/measure/MeasureCommand.java
@@ -1,29 +1,13 @@
 package tutorials.measure;
 
-import io.scif.services.DatasetIOService;
 import net.imagej.ImageJ;
 
-import org.scijava.Context;
 import org.scijava.command.Command;
-import org.scijava.display.event.input.KyEvent;
-import org.scijava.event.EventHandler;
-import org.scijava.io.IOService;
-import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
-import org.scijava.plugin.PluginInfo;
-import org.scijava.service.Service;
 
 import java.io.File;
-import java.util.List;
 
-/**
- * A command that generates a diagonal gradient image of user-given size.
- * <p>
- * For an even simpler command, see {@link HelloWorld} in this same
- * package!
- * </p>
- */
 @Plugin(type = Command.class, headless = true,
         menuPath = "Analyze>Batch Measure")
 public class MeasureCommand implements Command {
@@ -37,68 +21,27 @@ public class MeasureCommand implements Command {
     @Parameter(style = "directory")
     private File imageFolder;
 
-    /*
-     * This first @Parameter is a core ImageJ service (and thus @Plugin). The
-     * context will provide it automatically when this command is created.
-     */
-
     @Parameter
-    private DatasetIOService datasetIOService;
-
-    @Parameter
-    private IOService ioService;
-
-    //@Parameter
-    //private MeasureService measureService;
-
-    /*
-     * In this command, we will be using functions that can throw exceptions.
-     * Best practice is to log these exceptions to let the user know what went
-     * wrong. By using the LogService to do this, we let the framework decide
-     * the best place to display thrown errors.
-     */
-    @Parameter
-    private LogService logService;
-
-    @Parameter
-    private ImageJ imageJ;
-
-    @EventHandler
-    public void onEvent(final KyEvent evt) {
-        System.out.println(evt.getCode());
-    }
+    private MeasureService measureService;
 
     @Override
     public void run() {
-        MeasureService measureService = imageJ.get(MeasureService.class);
-        measureService.setMeasureBatchRunning(true);
+        measureService.startMeasureBatch();
 
-        try {
-            // load the image
-            for (File file : imageFolder.listFiles()) {
-                if (datasetIOService.canOpen(file.getAbsolutePath())) {
-                    // Dataset image = datasetIOService.open(file.getAbsolutePath());
-                    // Img img = (Img) ioService.open(file.getAbsolutePath());
-                    // imageJ.ui().show(img);
-                }
-            }
+        for (File file : imageFolder.listFiles()) {
+            measureService.addFile(file);
+            // Sort and randomise logic
         }
-        catch (final Exception exc) {
-            logService.error(exc);
-        }
+
+        measureService.setOutputFile(new File(imageFolder.getPath() + "/measurements.csv"));
+        measureService.nextImage();
     }
 
-    /** Tests our command. */
+    /** Tests the command. */
     public static void main(final String... args) throws Exception {
-        // Launch ImageJ as usual.
+        // Launch ImageJ for test run.
         final ImageJ ij = new ImageJ();
         ij.launch(args);
-        List<PluginInfo<Service>> services = ij.plugin().getPluginsOfType(Service.class);
-        for(PluginInfo service : services) {
-            System.out.println(service.getPluginClass());
-        }
-        Service s = ij.context().getService(MeasureService.class);
-        System.out.println(s);
     }
 
 }
--- maven-projects/measure/src/main/java/tutorials/measure/MeasureService.java
+++ maven-projects/measure/src/main/java/tutorials/measure/MeasureService.java
@@ -1,35 +1,169 @@
 package tutorials.measure;
 
+import io.scif.services.DatasetIOService;
+import net.imagej.DatasetService;
 import net.imagej.ImageJService;
+import net.imagej.display.OverlayView;
+import net.imagej.overlay.LineOverlay;
+import net.imglib2.img.Img;
+import org.scijava.command.CommandService;
+import org.scijava.display.Display;
+import org.scijava.display.DisplayService;
+import org.scijava.display.event.input.KyEvent;
+import org.scijava.display.event.window.WinClosedEvent;
+import org.scijava.display.event.window.WinClosingEvent;
+import org.scijava.event.EventHandler;
+import org.scijava.input.KeyCode;
+import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.service.AbstractService;
 import org.scijava.service.Service;
+import org.scijava.ui.UIService;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Plugin(type = Service.class)
 public class MeasureService extends AbstractService implements ImageJService {
+    @Parameter
+    private DatasetIOService ioService;
+
+    @Parameter
+    private DisplayService displayService;
+
+    @Parameter
+    private UIService uiService;
 
     private boolean measureBatchRunning;
-    private ArrayList<File> files;
+    private List<File> files;
+    private File outputFile;
+    private int currentFileIndex;
+    private Img currentImg;
+
+    private Map<String, List<Double>> results;
+
     // pixel per 1mm
     private int scale;
+
+    public void setOutputFile(File outputFile) {
+        this.outputFile = outputFile;
+    }
+
+    @Override
+    public void initialize() {
+        this.files = new ArrayList<>();
+        this.results = new HashMap<>();
+    }
+
+    @EventHandler
+    public void onKeyEvent(final KyEvent evt) {
+        if (evt.getModifiers().isMetaDown() && evt.getCode() == KeyCode.W) {
+            this.measure();
+        }
+    }
+
+    @EventHandler
+    public void onWindowClosingEvent(final WinClosingEvent evt) {
+        this.measure();
+    }
+
+    private void measure() {
+        Display<?> display = displayService.getActiveDisplay();
+        ArrayList<Double> measurements = new ArrayList<>();
+        results.put(currentFile().getName(), measurements);
+        if (display.toString().equals(currentFile().getName())) {
+            for (Object o : display) {
+                if (o instanceof OverlayView) {
+                    OverlayView view = (OverlayView) o;
+                    if (view.getData() instanceof LineOverlay) {
+                        LineOverlay line = (LineOverlay) view.getData();
+                        measurements.add(calculateLineLength(line));
+                    }
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onWindowClosedEvent(final WinClosedEvent evt) {
+        Display<?> display = evt.getDisplay();
+        if (display.toString().equals(currentFile().getName())) {
+            saveResults();
+            nextImage();
+        }
+    }
+
+    private void saveResults() {
+        try {
+            FileWriter writer = new FileWriter(this.outputFile);
+            for (Map.Entry<String, List<Double>> entry : this.results.entrySet()) {
+                StringBuilder line = new StringBuilder(entry.getKey());
+                for (Double measurement : entry.getValue()) {
+                    line.append(",").append(measurement);
+                }
+                writer.append(line.toString());
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private double calculateLineLength(LineOverlay line) {
+        double[] lineStart = new double[2];
+        line.getLineStart(lineStart);
+        double[] lineEnd = new double[2];
+        line.getLineEnd(lineEnd);
+
+        double a = Math.abs(lineStart[0] - lineEnd[0]);
+        double b = Math.abs(lineStart[1] - lineEnd[1]);
+
+        return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    }
 
     public boolean isMeasureBatchRunning() {
         return measureBatchRunning;
     }
 
-    public void setMeasureBatchRunning(boolean measureBatchRunning) {
-        System.out.println(measureBatchRunning);
-        this.measureBatchRunning = measureBatchRunning;
+    public void startMeasureBatch() {
+        this.currentFileIndex = -1;
+        this.measureBatchRunning = true;
+        System.out.println("run Forest, run!");
     }
 
-    public ArrayList<File> getFiles() {
-        return files;
+    public void endMeasureBatch() {
+        this.measureBatchRunning = false;
+        this.files = new ArrayList<>();
+        this.results = new HashMap<>();
     }
 
-    public void setFiles(ArrayList<File> files) {
-        this.files = files;
+    public void addFile(File file) {
+        this.files.add(file);
+    }
+
+    public File currentFile() {
+        if (this.currentFileIndex == -1) {
+            return null;
+        }
+        return this.files.get(this.currentFileIndex);
+    }
+
+    public void nextImage() {
+        this.currentFileIndex++;
+        if (currentFileIndex > this.files.size() - 1) {
+            this.endMeasureBatch();
+            return;
+        }
+        try {
+            this.currentImg = ioService.open(currentFile().getAbsolutePath());
+            uiService.show(this.currentImg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
